0c04719044828f4618a1c2f1aa5a78fa3013c939
YG7821 <yogyata_yogita@yahoo.co.in>
2026-05-19 21:46:58 +0530

    Add Terraform configuration for RDS MySQL instance

---
+++ rds/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  identifier           = "yogitadb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
